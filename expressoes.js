5ed38305aef665eb64c556bba9a6c1c02c9cb110
Gustimm <you@example.com>
2022-11-09 20:18:16 -0300

    Operadores e funcoes

---
+++ expressoes.js
@@ -0,0 +1,28 @@
+// function minhaFuncao(param) {
+//     //bloco de codigo
+// }
+
+// minhaFuncao("param")
+
+// expressão de funcao
+
+// const soma = function(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(soma(1, 1))
+
+// diferença principal ????
+// funções e var são "listadas" no topo do arquivo.
+
+console.log(apresentar())
+
+function apresentar() {
+    return "olá"
+}
+
+
+const soma = function(num1, num2) {
+    return num1 + num2
+}
+console.log(soma(1, 1))
---
+++ funcoes.js
@@ -0,0 +1,26 @@
+// let x = "";
+// console.log(x)
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO - PRIMEIRO TIPO DE ESCREVER UAM FUNÇÃO NO JS
+
+// 1) declara a função
+                      //string
+ function imprimeTexto(texto) {
+     console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("Oi!")
+// imprimeTexto("Outro Texto")
+
+// Três formas de escrever funções
+
+function soma(){
+    //return sempre tem que ser o ultimo codigo a ser criado.
+    return 2 + 2;
+}
+
+//console.log(soma())
